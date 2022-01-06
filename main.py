5d97cad0c0e2501858426cb8dcce6ad0a42be24f
Jan Bechtel <jan.bechtel@gmail.com>
2022-01-06 09:55:52 +0100

    mod for denon

--- main.py
+++ main.py
@@ -31,42 +31,15 @@ class Shop:
 #     "span",
 #     "a-size-medium a-color-price")
 
-otto = Shop(
-    "Otto",
-    "https://www.otto.de/p/playstation-5-1154028000/",
-    "PlayStation 5 Medienfernbedienung",
-    "h1",
-    "js_shortInfo__variationName prd_shortInfo__variationName")
+denon = Shop(
+    "Denon",
+    "https://www.denon.com/de-de/product/special/avc-x3700h-r/",
+    "Momentan nicht verfügbar",
+    "button",
+    "btn")
 
-mm = Shop(
-    "MediaMarkt",
-    "https://www.mediamarkt.de/de/product/_sony-playstation%C2%AE5-2661938.html",
-    "Dieser Artikel ist aktuell nicht verfügbar.",
-    "h4",
-    "Typostyled__StyledInfoTypo-sc-1jga2g7-0 dXEylL")
 
-expert = Shop(
-    "Expert",
-    "https://www.expert.de/shop/unsere-produkte/gaming-freizeit/sony-playstation/playstation-konsolen/11364129744-playstation-r-5.html",
-    "Das von Ihnen ausgewählte Produkt ist ausverkauft.",
-    "div",
-    "widget widget-Text widget-Text---c643eae2-bf63-4f6b-8975-4c2342b06cf3 widget-Text---view-text widget-Text---preset-default margin-xs-top-70 margin-lg-top-0")
-
-gamestop = Shop(
-    "GameStop",
-    "https://www.gamestop.de/PS5/Games/58665#",
-    "Derzeit nicht Verfügbar",
-    "a",
-    "megaButton buyDisabled")
-
-alternate = Shop(
-    "Alternate",
-    "https://www.alternate.de/Sony-Interactive-Entertainment/PlayStation-5-Digital-Edition-Spielkonsole/html/product/1676873",
-    "Artikel kann nicht gekauft werden",
-    "p",
-    "stockStatus noSeparateSale")
-
-Shops = [otto, expert, gamestop, alternate]
+Shops = [denon]
 
 
 def checkShop(name, url, nonavibstr, attrib, classname):
@@ -83,12 +56,12 @@ def checkShop(name, url, nonavibstr, attrib, classname):
                 if (value.strip()) != nonavibstr:
                     ava = "YES"
                     telegram_send.send(
-                        messages=["PS5 Verfügbar bei " + name + " Klick hier: " + url])
+                        messages=["Artikel Verfügbar bei " + name + " Klick hier: " + url])
         except:
             ava = "Bot detected"
     return ava
 
-print("Lurking for PS5 availibility...")
+print("Lurking for Article availibility...")
 print([i.name for i in Shops])
 while True:
 
